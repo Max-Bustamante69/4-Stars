bbc3a10051062c3c0e3fc06abe6e04ff0ec8184a
Max-Bustamante69 <mbustamang@eafit.edu.co>
2024-09-04 12:54:38 -0500

    DEBUg toolbar fix

--- fourStars/urls.py
+++ fourStars/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
-from debug_toolbar.toolbar import debug_toolbar_urls
-from . import views 
+from django.urls import path, include
+from . import views
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('professors/', views.professors, name='professors'),
     path('professorView/<int:professor_id>/', views.professor_view, name='professor_view'),
-    path('signUp/', views.signupScreen, name='signupScreen'),] + debug_toolbar_urls(),
+    path('signUp/', views.signupScreen, name='signupScreen'),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+
     
